80fdfb997f8ad36a2ed6159d4e820ca04ca0a542
Вадим <vadastashin@yandex.ru>
2021-01-28 16:46:25 +0300

    compare update

---
+++ js/dist/script.dev.js
@@ -0,0 +1,21 @@
+"use strict";
+
+$(document).ready(function () {
+  $('.compare-slider').slick({
+    slidesToShow: 3,
+    slidesToScroll: 3
+  });
+});
+var slideIndex = 3;
+$('.js-add-slide').on('click', function () {
+  slideIndex++;
+  $('.compare-slider').slick('slickAdd', '<div>' + slideIndex + '</div>');
+});
+$('.js-remove-slide').on('click', function () {
+  $('.compare-slider').slick('slickRemove', slideIndex - 1);
+
+  if (slideIndex !== 0) {
+    slideIndex--;
+  }
+});
+//# sourceMappingURL=script.dev.js.map
