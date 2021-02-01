1c16ff92895ba77bbaf8007a9a145fb07abec38f
Вадим <vadastashin@yandex.ru>
2021-02-01 10:00:15 +0300

    product_list update

--- js/product_filter.js
+++ js/product_filter.js
@@ -1,5 +1,6 @@
 $(document).ready(function(){
-	
-	
-	
-});
+	$("#btn-form").click(function(){
+		$(this).text($(this).text() == 'Показать' ? 'Скрыть' : 'Показать');
+		$("#rest-form").slideToggle();
+	});
+});	
